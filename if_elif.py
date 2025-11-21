dfc9efd3f720c72a8bbb810e489549bfb60f054e
phsp-manpreet <manpreet@pahasp.com>
2025-11-21 14:37:39 +0000

    changes in if_elif

--- if_elif.py
+++ if_elif.py
@@ -2,7 +2,7 @@ a=10
 b=10
 if a > b:
     print("a is greater value than b")
-elif a < b:
+elif (a< b):
     print("a is less than b")
-else a == b:
-    print("a is equal to b")
+else (a==b):
+    print("a is equal to the value of b")
