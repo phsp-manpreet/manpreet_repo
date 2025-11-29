1b3cdcb3b5442223e2f5190bf689c83488454fb9
phsp-sumit <sumit@pahasp.com>
2025-11-29 12:54:53 +0530

    Add else clause to if statement in if.py
    Added an else clause to handle the case when a is not greater than b.

--- if.py
+++ if.py
@@ -2,7 +2,10 @@ a=77
 b=20
 if a > b:
     print("a >b")
+else:
+    print("nit working")
 
  print("program ended successfully") 
  print("program ended")  # This line is not indented correctly
+
 
