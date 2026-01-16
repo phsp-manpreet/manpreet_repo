3be284e15aeb945c96e6bd90c13de6ec4f68c5e0
phsp-manpreet <manpreet@pahasp.com>
2026-01-16 08:16:30 +0000

    first

--- if.py
+++ if.py
@@ -2,7 +2,6 @@ a=77
 b=20
 if a > b:
     print("a is greater than b")
-
- print("program ended successfully") 
- print("program ended")  # This line is not indented correctly
+    print("program ended successfully") 
+    print("program ended")  # This line is not indented correctly
 
